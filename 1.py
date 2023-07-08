f01a016e0752684769920605ff8c0ea1343be8a1
Dhairya Patel <102598850+DhairyaPatel1403@users.noreply.github.com>
2023-07-08 16:14:26 +0530

    Update 1.py with subtract function

--- 1.py
+++ 1.py
@@ -1,6 +1,12 @@
 def add_num(a,b):
     sum=a+b;
     return sum;
+
+def subtract_num(a,b):
+    subtract = a-b;
+    return subtract
+
 num1=int(input(" number one: "))
 num2=int(input("number two :"))
 print("The sum is",add_num(num1,num2))
+print("The sum is",subtract_num(num1,num2))
